1064ba991dc1dbc9d16b907dd461d2799cfd57dd
Chris Staley <skeletorsue@gmail.com>
2016-02-28 18:18:03 -0700

    Changed the colors around for my devices. Updated the blue pin to something else Made the color change have a fade effect. Improved the boot up color tests.

--- ir-rgb-light-controller/ir-rgb-light-controller.ino
+++ ir-rgb-light-controller/ir-rgb-light-controller.ino
@@ -2,26 +2,29 @@
 
 int RECV_PIN = 11;
 int RED_PIN = 5;
+int RED_PIN_VALUE = 0;
 int GREEN_PIN = 6;
-int BLUE_PIN = 3;
-int FADE_SPEED = 5;
+int GREEN_PIN_VALUE = 0;
+int BLUE_PIN = 9;
+int BLUE_PIN_VALUE = 0;
+int FADE_SPEED = 3;
 
 unsigned long PORTS[6] = {
-  0x0000000, // off
+  0x5EA17887, // off
   0x1FEA05F, // computer
   0x1FEE01F, // xbox 360
   0x1FE10EF, // wii
   0x1FE906F, // xbox one
-  0x1FED827
+  0x1FED827  // chromecast
 };
 
 char* COLORS[6] = {
-  "000000",
-  "ff9900",
-  "00ff00",
-  "cc00cc",
-  "0000ff",
-  "ff0000"
+  "000000", // black
+  "00ff00", // green
+  "0000ff", // blue
+  "95FFFF", // light blue
+  "0000ff", // blue
+  "ff0000"  // red
 
 };
 
@@ -34,12 +37,12 @@ void setup() {
 
   Serial.println("Hello world!");
 
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
   // used to test the lights
-  for (int i = 0; i < 6; i++) {
-    setLights(COLORS[i]);
-    delay(500);
-  }
-  setLights("000000");
+  lightTest();
 }
 
 void loop() {
@@ -88,5 +91,88 @@ void setLights(char* color_code) {
   p("r: %d", r_dec);
   p("g: %d", g_dec);
   p("b: %d", b_dec);
+
+  while (RED_PIN_VALUE != r_dec || GREEN_PIN_VALUE != g_dec || BLUE_PIN_VALUE != b_dec) {
+    if (r_dec != RED_PIN_VALUE) {
+      if (r_dec < RED_PIN_VALUE ) {
+        setRed(RED_PIN_VALUE - 1);
+      } else {
+        setRed(RED_PIN_VALUE + 1);
+      }
+    }
+
+    if (g_dec != GREEN_PIN_VALUE) {
+      if (g_dec < GREEN_PIN_VALUE ) {
+        setGreen(GREEN_PIN_VALUE - 1);
+      } else {
+        setGreen(GREEN_PIN_VALUE + 1);
+      }
+    }
+
+    if (b_dec != BLUE_PIN_VALUE) {
+      if (b_dec < BLUE_PIN_VALUE ) {
+        setBlue(BLUE_PIN_VALUE - 1);
+      } else {
+        setBlue(BLUE_PIN_VALUE + 1);
+      }
+    }
+    delay(FADE_SPEED);
+  }
+
+}
+
+void setRed(int val) {
+  RED_PIN_VALUE = val;
+  analogWrite(RED_PIN, val);
+}
+
+void setGreen(int val) {
+  GREEN_PIN_VALUE = val;
+  analogWrite(GREEN_PIN, val);
+}
+
+void setBlue (int val) {
+  BLUE_PIN_VALUE = val;
+  analogWrite(BLUE_PIN, val);
+}
+
+void lightTest() {
+  Serial.println("Starting light test sequence");
+  int i;
+
+  for (i = 0; i < 256; i++) {
+    setRed(i);
+    delay(5);
+  }
+  for (i = 255; i >= 0; i--) {
+    setRed(i);
+    delay(5);
+  }
+
+  for (i = 0; i < 256; i++) {
+    setGreen(i);
+    delay(5);
+  }
+  for (i = 255; i >= 0; i--) {
+    setGreen(i);
+    delay(5);
+  }
+
+  for (i = 0; i < 256; i++) {
+    setBlue(i);
+    delay(5);
+  }
+  for (i = 255; i >= 0; i--) {
+    setBlue(i);
+    delay(5);
+  }
+
+  for (int i = 0; i < 6; i++) {
+    setLights(COLORS[i]);
+  }
+
+  setLights("000000");
+
+  Serial.println("Light test sequence has completed, how did it look?");
 }
 
